0a56bb80a59c7719918cc79b273602303121fa7c
Civic Sites <tmepphoto@gmail.com>
2026-08-15 18:25:25 -0400

    Add local business structured data

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,32 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
 };
+const businessSchema = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  name: "Islamorada Coastal Adventures",
+  url: "https://www.islamoradacoastaladventures.com/",
+  logo: "https://www.islamoradacoastaladventures.com/logo.JPG",
+  image: "https://www.islamoradacoastaladventures.com/logo.JPG",
+  telephone: "+1-305-735-4070",
+  email: "Matt@islamoradacoastaladventures.com",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "85920 Overseas Hwy",
+    addressLocality: "Islamorada",
+    addressRegion: "FL",
+    postalCode: "33036",
+    addressCountry: "US",
+  },
+  areaServed: {
+    "@type": "Place",
+    name: "Islamorada, Florida Keys",
+  },
+  sameAs: [
+    "https://www.facebook.com/IslamoradaCoastalAdventures",
+    "https://www.instagram.com/islamorada_coastal_adventures/",
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -33,7 +59,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+     <body className="min-h-full flex flex-col">
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{ __html: JSON.stringify(businessSchema) }}
+  />
+  {children}
+</body>
     </html>
   );
 }
